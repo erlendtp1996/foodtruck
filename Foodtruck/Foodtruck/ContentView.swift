ff5a7c0fff270d33bc39683c472aebd3bb69317a
Thomas Erlendson <erlendtp1996@gmail.com>
2020-04-05 15:15:01 -0400

    First Commit

---
+++ Foodtruck/Foodtruck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foodtruck
+//
+//  Created by Thomas Erlendson on 4/5/20.
+//  Copyright © 2020 Thomas Erlendson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Foodtrucks!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
